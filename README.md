826b3b6f15ac139ed04c947e9d97633a4ebdfc51
TrevorWu <wowpapa3232@gmail.com>
2020-08-02 00:48:36 +0800

    Init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # DHT11_Api
-An RESTful api that can return the DHT11 sensor's data, and an android app.
+A RESTful api that can return the DHT11 sensor's data, and an android app can work with it.
+
+## server.py
+Run this python script on Raspberry Pi.<br>
+Remember to change the ip / port.
+
+## Folder: AndroidClient
+An android app that can work with the server.py.<br>
+Visit the folder for more information.
---
+++ server.py
@@ -0,0 +1,28 @@
+import board
+import adafruit_dht
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+last_measurement_time = None
+dhtDevice = adafruit_dht.DHT11(board.D14)
+
+@app.route('/api/v1/temp+hum', methods=['GET'])
+def get_temperature_and_humidity():
+    global temperature_c
+    global humidity
+    try:
+        temperature_c = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+        return jsonify({
+            'temperature': temperature_c, 
+            'humidity': humidity
+        })
+ 
+    except RuntimeError as error:
+        return jsonify({
+            'temperature': 0, 
+            'humidity': 0
+        })
+
+if __name__ == '__main__':
+    app.run(host='192.168.1.138', port=5001)
